f9560f97953f43cb873022d643450c1ac4361db0
Richard Hunter <richard@richardhunter.co.uk>
2016-02-22 20:59:15 +0000

    changing spaces to tabs in app.js

--- public/js/app.js
+++ public/js/app.js
@@ -9,79 +9,65 @@
  * Main module of the application.
  */
 angular
-  .module('holidaySiteApp', [
-    'ngAnimate',
-    'ngAria',
-    'ngCookies',
-    'ngMessages',
-    'ngResource',
-    'ui.router',
-    'ngSanitize',
-    'ngTouch',
-    'uiGmapgoogle-maps',
-    'header',
-    'articles',
-    'carousel',
-    'harbours'
+	.module('holidaySiteApp', [
+		'ngAnimate',
+		'ngAria',
+		'ngCookies',
+		'ngMessages',
+		'ngResource',
+		'ui.router',
+		'ngSanitize',
+		'ngTouch',
+		'uiGmapgoogle-maps',
+		'header',
+		'articles',
+		'carousel',
+		'harbours'
+	])
+	.controller('MainCtrl', ['$scope', function ($scope) {
+		$scope.carouselImages = ['dunoon1.jpeg', 'dunoonpier.jpg', 'oldDunoon.jpg', 'westbay.jpg'];
+		$scope.mainImage = $scope.carouselImages[1];
+	}])
+	.controller('ContactCtrl', ['$scope', function($scope) {
+		$scope.user = {};
+		$scope.formMode = 'form';
+		$scope.submit = function () {
+			$scope.formMode = 'results';
+		}}])
+	.controller('PlacesCtrl', ['$scope', '$stateParams','ArticleService', function ($scope, $stateParams, articlesService) {
+		var defaultArticle = { text : '<p>Sorry, there is no article for that place</p>' };
 
-  ])
-    .controller('MainCtrl', ['$scope', function ($scope) {
-        $scope.carouselImages = ['dunoon1.jpeg', 'dunoonpier.jpg', 'oldDunoon.jpg', 'westbay.jpg'];
-        $scope.mainImage = $scope.carouselImages[1];
-    }])
-
-    .controller('ContactCtrl', ['$scope', function($scope) {
-
-        $scope.user = {};
-
-        $scope.formMode = 'form';
-
-        $scope.submit = function () {
-            $scope.formMode = 'results';
-        }
-    }])
-
-    .controller('PlacesCtrl', ['$scope', '$stateParams','ArticleService', function ($scope, $stateParams, articlesService) {
-
-        var defaultArticle = { text : '<p>Sorry, there is no article for that place</p>' };
-
-        articlesService.getArticleByKeyword($stateParams.place).then(function(article) {
-            if(!article.data || !article.data.text) {
-                $scope.currentArticle = defaultArticle;
-            } else {
-                $scope.currentArticle = article.data;
-            }
-        },function() {
-            $scope.currentArticle = defaultArticle;
-        });
-
-    }])
-
-    .config(
-    ['$stateProvider',
-        function ($stateProvider) {
-
-            $stateProvider
-
-                .state("home", {
-                    url: "/",
-                    templateUrl: 'views/main.html',
-                    controller : 'MainCtrl'
-                })
-                .state('places', {
-                    url : '/places',
-                    templateUrl: 'views/places.html',
-                    controller: 'MapCtrl'
-                })
-                .state('places.place', {
-                    url : '/:place',
-                    templateUrl : 'views/place.html',
-                    controller : 'PlacesCtrl'
-                })
-                .state('contact', {
-                    url : '/contact',
-                    templateUrl: 'views/contact.html',
-                    controller: 'ContactCtrl'
-                });
-
-  }]);
+		articlesService.getArticleByKeyword($stateParams.place).then(function(article) {
+				if(!article.data || !article.data.text) {
+						$scope.currentArticle = defaultArticle;
+				} else {
+						$scope.currentArticle = article.data;
+				}
+		},function() {
+				$scope.currentArticle = defaultArticle;
+		});
+	}])
+	.config(['$stateProvider',
+		function ($stateProvider) {
+			$stateProvider
+				.state("home", {
+					url: "/",
+					templateUrl: 'views/main.html',
+					controller : 'MainCtrl'
+				})
+				.state('places', {
+					url : '/places',
+					templateUrl: 'views/places.html',
+					controller: 'MapCtrl'
+				})
+				.state('places.place', {
+					url : '/:place',
+					templateUrl : 'views/place.html',
+					controller : 'PlacesCtrl'
+				})
+				.state('contact', {
+					url : '/contact',
+					templateUrl: 'views/contact.html',
+					controller: 'ContactCtrl'
+				});
+	}]);
